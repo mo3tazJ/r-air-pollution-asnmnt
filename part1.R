79f2a8dd5c27054efb4b0e424e39d6c355de493b
mo3tazJ <en.mo3taz@gmail.com>
2025-05-13 03:41:12 +0300

    correcting errors and refining

--- part1.R
+++ part1.R
@@ -1,8 +1,9 @@
 # Write a function named 'pollutantmean' that calculates the mean of a pollutant across a specified list of monitors.
 pollutantmean <- function(directory, pollutant, id) {
     files_list <- list.files(directory, full.names = T)
-    tmp <- lapply(files_list, read.csv)
+    ops <- files_list[id]
+    tmp <- lapply(ops, read.csv)
     output <- do.call(rbind, tmp)
-    dat <- output[id, pollutant]
+    dat <- output[, pollutant]
     mean(dat, na.rm = T)
 }

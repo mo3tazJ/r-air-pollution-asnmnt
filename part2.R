5ba852e0f43505c38adf3170e855a4b952c4bdda
mo3tazJ <en.mo3taz@gmail.com>
2025-05-22 19:39:40 +0300

    Finalizing - Adding Comments

--- part2.R
+++ part2.R
@@ -1,10 +1,11 @@
+# "complete" is function which return a data frame of the name of the file and the number of complete cases
 complete <- function(directory, id = 1:332) {
     # Initialize vectors to hold the results
     ids <- integer()
     nobs <- integer()
     # Reading Files Names
     files_list <- list.files(directory, full.names = TRUE)
-    # Subsetting the id corresponding files
+    # Sub-setting the corresponding files by the provided param "id"
     obs <- files_list[id]
     for (i in id) {
         # Read the data from CSV file
